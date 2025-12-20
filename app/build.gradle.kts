8977ea3176a672b9b07715f8cc1aeff376f6647e
Yash <khatri7yash@gmail.com>
2025-12-20 20:04:25 +0530

    - Added tests for popular screen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     testImplementation(libs.androidx.paging.testing)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.mockk.io)
+    androidTestImplementation(libs.mockk.io.android)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.hilt.android.test)
--- app/src/main/java/com/example/mvvm_compose_di/ui/screens/popular/PopularScreen.kt
+++ app/src/main/java/com/example/mvvm_compose_di/ui/screens/popular/PopularScreen.kt
@@ -1,23 +1,45 @@
 package com.example.mvvm_compose_di.ui.screens.popular
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.mvvm_compose_di.navigation.NavScreens
+import com.example.mvvm_compose_di.ui.component.Movies
+import com.example.mvvm_compose_di.ui.component.base.BaseColumn
 import com.example.mvvm_compose_di.ui.screens.base.BaseScreen
 
 
 @Composable
 fun PopularScreen(navigation: (NavScreens?, Array<out Any>?) -> Unit){
     val title by remember { mutableStateOf(NavScreens.Popular.title) }
+    val viewModel = hiltViewModel<PopularViewModel>()
+    val popularMovies = viewModel.popularMovies.collectAsLazyPagingItems()
+    val loadingState by viewModel.loadingState.collectAsState()
+
+
+    LaunchedEffect(Unit) {
+        viewModel.getPopularMovies()
+    }
+
+    LaunchedEffect(popularMovies.loadState) {
+        viewModel.updateState(popularMovies.loadState.refresh)
+    }
+
     BaseScreen (
         title = title,
         navigation = navigation
     ){
-//        BaseColumn(state = ) {
-//
-//        }
+        BaseColumn(state = loadingState, modifier = Modifier ) {
+            Movies(moviesItems = popularMovies){
+
+            }
+        }
     }
 
 }
