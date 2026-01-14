2a59d4ac616b619896763539e98317ef981a36fb
Yash <khatri7yash@gmail.com>
2026-01-14 23:25:16 +0530

    - Added shared flow's stateIn example

--- extlib/src/main/java/com/example/extlib/SharedFlows.kt
+++ extlib/src/main/java/com/example/extlib/SharedFlows.kt
@@ -1,16 +1,48 @@
 package com.example.extlib
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
-        normalConsumer()
+//        normalConsumer()
+        consumerUsingSharedIn()
     }
+}
+
+private suspend fun consumerUsingSharedIn() = coroutineScope {
+    launch {
+        getSharedFlow().collect {
+            println("Receiver A: $it")
+        }
+    }
+}
+
+private fun CoroutineScope.getSharedFlow(): SharedFlow<Int> {
+    val list = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val sharedIn = flow<Int> {
+        list
+            .subList(0, 5)
+            .forEach {
+                println("Producing $it")
+                emit(it)
+                delay(1000)
+            }
+    }.shareIn(
+        this,
+        SharingStarted.WhileSubscribed(5000),
+        0
+    )
+    return sharedIn
 }
 
 private suspend fun normalConsumer() = coroutineScope {
